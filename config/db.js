30e0ee6d3e5bf882234fb55a78f6e2e01c56d183
Fábio Marques <fabio@ua.pt>
2025-03-29 16:07:47 +0000

    The .env file should be a local configuration file. It was removed from the repository and created an .env.example file for example. To enable different db configurations extra variables were added (see .env.example). If more configuration variables are needed then they should be added to .env.example file for posterior use

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 // Configuração da conexão com a base de dados
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_estudos_estga_1mes',
-  port: 3307,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 // Conectar apenas uma vez ao inicializar
