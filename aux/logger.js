ba539ba526807e325fdfacca4dbfb6f0cd22eaed
Gonçalo Alves <57621816+Tugabeast@users.noreply.github.com>
2026-01-22 14:20:23 +0000

    Update logger.js

--- aux/logger.js
+++ aux/logger.js
@@ -1,13 +1,1 @@
 // Winston Logger Configuration
-const { createLogger, format, transports } = require('winston');
-
-const logger = createLogger({
-    level: 'silly',
-    format: format.json(),
-    transports: [
-        new transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new transports.File({ filename: 'logs/combined.log', level: 'silly' }),
-    ],
-});
-
-module.exports = { logger };
