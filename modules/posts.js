d7a38131b2ca8493e59af6422e9e045b91cb5eee
Gonçalo Alves <goncalo.alves@ua.pt>
2026-02-24 12:01:27 +0000

    Update posts.js

--- modules/posts.js
+++ modules/posts.js
@@ -485,14 +485,14 @@ router.get('/', async (req, res) => {
             logger.debug(`[POSTS - FEED] A executar query de histórico para o UserID: ${userId} (Estudo ID: ${targetStudyId})`);
             
             const historyQuery = `
-                SELECT DISTINCT p.*, s.name AS studyName, max(c.createdAt)
+                SELECT DISTINCT p.*, s.name AS studyName, max(c.createdAt) AS createdAtMax
                 FROM post p
                 JOIN study s ON p.studyId = s.id
                 JOIN classification c ON c.postId = p.id
                 WHERE p.studyId = ? 
                 AND c.userId = ?
                 GROUP BY p.id
-                ORDER BY c.createdAt DESC
+                ORDER BY createdAtMax DESC
             `;
             [posts] = await db.promise().query(historyQuery, [targetStudyId, userId]);
             
